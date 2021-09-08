6f8f333f4d09062aae0e8a5c59bef18d18886d04
odykai2009 <odykai2009@gmail.com>
2021-09-08 12:57:55 -0400

    .

--- api/run.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def main():
-    package_dir = os.path.dirname(os.path.abspath(__file__))
-    gunicorn_path = os.path.join(package_dir, "application", "gunicorn.conf")
-    cmd = f"gunicorn -c {gunicorn_path} sys_eval:app -t 3600"
-    os.system(cmd)
--- api/run_locally.py
+++
@@ -1,6 +0,0 @@
-from sys_eval import app
-
-# can access it by hitting IP: http://127.0.0.1:5000/
-
-def main():
-    app.run(host="0.0.0.0", debug=True)
